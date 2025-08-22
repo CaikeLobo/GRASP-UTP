25dd516cc1ac3954464901e6e2da712acb975074
Caike Lobo <131018342+CaikeLobo@users.noreply.github.com>
2025-08-22 17:23:57 -0300

    Add files via upload

---
+++ src/Professor.java
@@ -0,0 +1,23 @@
+public class Professor {
+    private int idProf;
+    private String nome;
+
+    public Professor(int idProf, String nome) {
+        this.idProf = idProf;
+        this.nome = nome;
+    }
+
+    // Getters
+    public int getIdProf() {
+        return idProf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + idProf + ", Nome: " + nome;
+    }
+}
